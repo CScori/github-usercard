53baa52e7a0e1199f29c0b63e513c2a54a54d87a
CScori <amilanotte1366@gmail.com>
2019-09-05 11:29:02 -0500

    adds new elements

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -33,7 +33,20 @@ axios.get(`https://api.github.com/users/your name`)
 */
 
 const followersArray = [];
-function ()
+function newCard (data) {
+  //adding new elements
+  const cardContainer = document.createElement('div');
+  const cardClass = createElement('div');
+  const cardImg = document.createElement('img');
+  const cardName = document.createElement('h3');
+  const cardUser = document.createElement('p');
+  const cardLocation = document.createElement('p');
+  cosnt cardAdd = document.createElement('p');
+  const follow = document.createElement('p');
+  const fling = document.createElement('p');
+  const bio = document.createElement('p');
+
+}
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
 
